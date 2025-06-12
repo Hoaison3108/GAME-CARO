6b488e5e169b94c2568b10af2ae00e12c8f54450
Hoaison3108 <hoaison3108@gmail.com>
2025-06-12 17:02:16 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace GAME_CARO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlChessBoar = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.PicAvatar = new System.Windows.Forms.PictureBox();
+            this.TxtPlayerName = new System.Windows.Forms.TextBox();
+            this.PrgCoolDown = new System.Windows.Forms.ProgressBar();
+            this.PicMark = new System.Windows.Forms.PictureBox();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.BtnLan = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicAvatar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMark)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PnlChessBoar
+            // 
+            this.PnlChessBoar.Location = new System.Drawing.Point(-1, 0);
+            this.PnlChessBoar.Name = "PnlChessBoar";
+            this.PnlChessBoar.Size = new System.Drawing.Size(781, 634);
+            this.PnlChessBoar.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.PicAvatar);
+            this.panel2.Location = new System.Drawing.Point(787, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(372, 293);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.BtnLan);
+            this.panel3.Controls.Add(this.txtIp);
+            this.panel3.Controls.Add(this.PicMark);
+            this.panel3.Controls.Add(this.PrgCoolDown);
+            this.panel3.Controls.Add(this.TxtPlayerName);
+            this.panel3.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(787, 299);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(372, 335);
+            this.panel3.TabIndex = 2;
+            // 
+            // PicAvatar
+            // 
+            this.PicAvatar.Location = new System.Drawing.Point(3, 4);
+            this.PicAvatar.Name = "PicAvatar";
+            this.PicAvatar.Size = new System.Drawing.Size(366, 286);
+            this.PicAvatar.TabIndex = 0;
+            this.PicAvatar.TabStop = false;
+            // 
+            // TxtPlayerName
+            // 
+            this.TxtPlayerName.Location = new System.Drawing.Point(14, 12);
+            this.TxtPlayerName.Name = "TxtPlayerName";
+            this.TxtPlayerName.Size = new System.Drawing.Size(177, 22);
+            this.TxtPlayerName.TabIndex = 0;
+            // 
+            // PrgCoolDown
+            // 
+            this.PrgCoolDown.Location = new System.Drawing.Point(14, 38);
+            this.PrgCoolDown.Name = "PrgCoolDown";
+            this.PrgCoolDown.Size = new System.Drawing.Size(177, 23);
+            this.PrgCoolDown.TabIndex = 1;
+            // 
+            // PicMark
+            // 
+            this.PicMark.Location = new System.Drawing.Point(198, 12);
+            this.PicMark.Name = "PicMark";
+            this.PicMark.Size = new System.Drawing.Size(163, 105);
+            this.PicMark.TabIndex = 2;
+            this.PicMark.TabStop = false;
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(14, 68);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(177, 22);
+            this.txtIp.TabIndex = 3;
+            // 
+            // BtnLan
+            // 
+            this.BtnLan.Location = new System.Drawing.Point(14, 94);
+            this.BtnLan.Name = "BtnLan";
+            this.BtnLan.Size = new System.Drawing.Size(177, 23);
+            this.BtnLan.TabIndex = 4;
+            this.BtnLan.Text = "button1";
+            this.BtnLan.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 35);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "5 in a line to win";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 634);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.PnlChessBoar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicAvatar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMark)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlChessBoar;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox PicAvatar;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox PicMark;
+        private System.Windows.Forms.ProgressBar PrgCoolDown;
+        private System.Windows.Forms.TextBox TxtPlayerName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnLan;
+        private System.Windows.Forms.TextBox txtIp;
+    }
+}
+
